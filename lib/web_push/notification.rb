ef6f9fed8f6b708649e9ac63f7052e5e849266c3
dnara <daijinara@gmail.com>
2026-04-27 20:48:56 +0700

    Deleted files in .github directory

--- lib/web_push/notification.rb
+++ lib/web_push/notification.rb
@@ -15,7 +15,7 @@ class WebPush::Notification
 
   private
     def vapid_identification
-      { subject: "mailto:support@37signals.com" }.merge \
+      { subject: "mailto:support@fathom-chat.work" }.merge \
         Rails.configuration.x.vapid.symbolize_keys
     end
 
